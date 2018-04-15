e66f01454d9899ef6bea17ac69399307617cd91e
Nick Kasten <kastencode@gmail.com>
2018-04-14 20:35:26 -0500

    added setDriverLocation()

--- controllers/driverController.js
+++ controllers/driverController.js
@@ -29,25 +29,8 @@ exports.getAllDrivers = async (req, res) => {
   try {
     let driverDocs = await driverModel.find();
 
-    if (req.query.available == 'true') {
-      driverDocs = driverDocs.filter(driver => driver.available == true);
-    }
-    if (req.query.maxDistance) {
-      // calculate disatnce here so we can filter it
-      const baseURL = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=";
-      const key = "&key=" + process.env.GOOGLE_KEY;
-      const destinations = "&destinations=30.229246,-97.725819";  // using hardcoded test address (E. Oltorf)
-      const origins = driverDocs.filter(driver => driver.location.latitude && driver.location.longitude).map(driver => String(driver.location.latitude) + ',' + String(driver.location.longitude)).join('|');
-      const requestString = baseURL + origins + destinations + key;
-      const distanceData = await getContent(requestString);
-      const newDriverDocs = driverDocs.filter(driver => driver.location.latitude && driver.location.longitude).map((driver, index) => {
-        return {
-          ...JSON.parse(JSON.stringify(driver)), 
-          distance : String(JSON.parse(distanceData).rows[index].elements[0].distance.text)
-        };
-      });
-      driverDocs = newDriverDocs.filter(driver => 
-        parseFloat(driver.distance.split(' ')[0]) <= req.query.maxDistance);
+    if (req.query.available) {
+      driverDocs = driverDocs.filter(driver => driver.available == JSON.parse(req.query.available));
     }
     if (req.query.minCapacity) {
       driverDocs = driverDocs.filter(driver => driver.capacity >= req.query.minCapacity);
@@ -77,6 +60,23 @@ exports.setAvailability = async (req, res) => {
   }
 }
 
+exports.setDriverLocation = async (req, res) => {
+  try {
+    if (!req.body.location.latitude || !req.body.location.longitude)
+      throw 'Error: Incomplete parameters.';
+    const updatedDriver = await driverModel.findByIdAndUpdate(
+      req.driver._id, 
+      { $set: { location: {
+          latitude: req.body.latitude,
+          longitude: req.body.longitude 
+      } } }, { new: true });
+    res.send(updatedDriver); // should probably just return 200 status for 'idempotency'
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(400);
+  }
+}
+
 exports.rateRider = async (req, res) => {
   if (req.driver) {
     try {
@@ -95,7 +95,7 @@ exports.rateRider = async (req, res) => {
 
 exports.addDriver = async (req, res) => {
   const credentials = auth(req);
-  if (credentials && credentials.name == "admin" && credentials.pass == "password" &&
+  if (credentials && credentials.name == 'admin' && credentials.pass == 'password' &&
       req.body.name && req.body.vehicle && req.body.capacity) {
     try {
       const newDriver = new driverModel({
@@ -123,7 +123,7 @@ exports.addDriver = async (req, res) => {
 
 exports.removeDriver = async (req, res) => {
   const credentials = auth(req);
-  if (credentials && credentials.name == "admin" && credentials.pass == "password") {
+  if (credentials && credentials.name == 'admin' && credentials.pass == 'password') {
     try {
       const driver = await driverModel.remove({ _id : req.driver._id });
       res.sendStatus(200);
