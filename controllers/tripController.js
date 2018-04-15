517083c1630ee5d7c5b16870da9d8e6a006aae31
Nick Kasten <kastencode@gmail.com>
2018-04-15 16:43:20 -0500

    basic trip/directions simulation v0.1

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -10,13 +10,23 @@ exports.getCurrentRate = (req, res) => {
 exports.tripSimulation = async (req, res) => {
   try {
     let count = 0;
+    const tripData = await simulateTrip('test');
     const intervalID = setInterval(() => {
       console.log('testing timer');
-      count++;
-      if (count > 9) clearInterval(intervalID);
+      tripData.routes[0].legs.forEach(
+        leg => leg.steps.forEach(
+          step => {
+            console.log(`travel ${step.distance.text} for ${step.duration.text} to finish at ${step.end_location.lat},${step.end_location.lng}.`);
+            count++;
+          }
+        )
+      );
+      if (count > 9) {
+        clearInterval(intervalID);
+        console.log(`${count} steps complete.`);
+      }
     }, 1000);
-    const tripData = await simulateTrip('test');
-    tripData.routes[0].legs.forEach(leg=>leg.steps.forEach(step=>console.log(step)));
+    //tripData.routes[0].legs.forEach(leg=>leg.steps.forEach(step=>console.log(step)));
     res.sendStatus(200);
   } catch (e) {
     console.error(e.message || e);
