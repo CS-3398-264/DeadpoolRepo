823f739dbfda41b66043011639cb592244fe5ce6
Nick Kasten <kastencode@gmail.com>
2018-04-19 17:24:14 -0500

    updated test objects (all tests passing now)

--- utils/testObjects.js
+++ utils/testObjects.js
@@ -2,39 +2,55 @@ exports = module.exports = {};
 
 exports.testDriver = {
   "reviews": [
-      5,
-      4.6,
-      4,
-      4.7,
-      4.5,
-      4.9
+    {
+      "tripID": "5ad7fefd0b5bdad75e2ff1c3",
+      "score": 4.98
+  },
+  {
+      "tripID": "5ad80a9b3a2237da7bb4b534",
+      "score": 4.99
+  },
+  {
+      "tripID": "5ad80a9b3a2237da7bb4b53z",
+      "score": 2.99
+  }
   ],
   "_id": "5abdd27a734d1d0cf303e71f",
-  "name": "Test Driver",
+  "name": "Test Driver1",
   "vehicle": "testCar",
   "capacity": 3,
-  "available": false
+  "available": true,
+  "currentTrip": "none"
 };
 exports.testDriverNoReviews = {
   "reviews": [],
   "_id": "5abdd27a734d1d0cf303e71f",
-  "name": "Test Driver",
+  "name": "Test Driver2",
   "vehicle": "testCar",
-  "capacity": 3,
-  "available": false
+  "capacity": 5,
+  "available": false,
+  "currentTrip": "none"
 };
 exports.testRider = {
   "reviews": [
-      4.6,
-      5,
-      2,
-      4.5
+    {
+      "tripID": "5ad7fefd0b5bdad75e2ff1c3",
+      "score": 4.98
+  },
+  {
+      "tripID": "5ad80a9b3a2237da7bb4b534",
+      "score": 4.99
+  },
+  {
+      "tripID": "5ad80a9b3a2237da7bb4b53z",
+      "score": 2.99
+  }
   ],
   "_id": "5abdd2d6734d1d0cf303e742",
-  "name": "Test Rider"
+  "name": "Test Rider1"
 };
 exports.testRiderNoReviews = {
   "reviews": [],
   "_id": "5abdd2d6734d1d0cf303e742",
-  "name": "Test Rider"
+  "name": "Test Rider2"
 };
