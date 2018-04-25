3817ba076552ab9b0129b2f0cecf27fd1a522c89
Nick Kasten <kastencode@gmail.com>
2018-04-24 19:58:09 -0500

    adding cors support

--- server.js
+++ server.js
@@ -9,6 +9,23 @@ const app = express();
 app.get('/', (req, res) => { 
   res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+// allow CORS support
+app.use((req, res, next) => {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use('/api', apiRouter);
 
 app.listen(portNum);
